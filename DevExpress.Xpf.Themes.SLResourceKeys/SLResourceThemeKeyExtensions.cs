9dcdff814005728e28ec1b0691f790d8dc7dc1ed
Florian Koch <fkoch@selectline.de>
2016-09-06 08:01:54 +0200

    Added project

---
+++ DevExpress.Xpf.Themes.SLResourceKeys/SLResourceThemeKeyExtensions.cs
@@ -0,0 +1,8 @@
+﻿using DevExpress.Xpf.Utils.Themes;
+
+namespace DevExpress.Xpf.Themes.SLResourceKeys
+{
+    public class SLResourceThemeKeyExtensions : ThemeKeyExtensionBase<SLResourceThemeKeys>
+    {
+    }
+}
---
+++ DevExpress.Xpf.Themes.SLResourceKeys/SLResourceThemeKeys.cs
@@ -0,0 +1,18 @@
+﻿namespace DevExpress.Xpf.Themes.SLResourceKeys
+{
+    public enum SLResourceThemeKeys
+    {
+        GridRowBackgroundAlternateNoneColor,
+        GridRowBackgroundAlternateSelectedColor,
+        RibbonPageCategoryHeaderBackground,
+        LayoutGroupTabbedLeftHeight,
+        LayoutGroupTabbedLeftMaxWidth,
+        ComboButtonButtonWidth,
+        ButtonEditButtonWidth,
+        GroupBoxPanelBackground,
+        GroupBoxPanelForeground,
+        GroupBoxPanelBottomBorder,
+        GroupBoxPanelHeight,
+        GroupBoxPanelMargin
+    }
+}
